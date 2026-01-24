8a1b28a8a132aa2a7eb4d567793174205fe11f31
Bot de Bitbucket <y1308diny8odzq8ak3mfdrqggap6vj@bots.bitbucket.org>
2026-01-24 16:06:08 +0100

    Subida cambio para pages

--- 2DAW/2DAW-DWC/ValorantApi/navbar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const currentPage = window.location.pathname.split("/").pop();
-  const navLinks = {
-    "index.html": document.getElementById("navInicio"),
-    "personajes.html": document.getElementById("navPersonajes"),
-    "armas.html": document.getElementById("navArmas")
-  };
-  if (navLinks[currentPage]) navLinks[currentPage].classList.add("border-b-4","border-red-600","rounded-sm");
-});
